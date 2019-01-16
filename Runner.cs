147238a0db75a5042404a26074a5868772106a71
Devinvis <devinvis@protonmail.com>
2019-01-16 20:46:34 +0300

    Запускатор

---
+++ Runner.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalcGeometricFigure
+{
+    public class Runner
+    {
+        IFigure _figure;
+        public Runner(IFigure figure)
+        {
+            _figure = figure;
+        }
+
+        /// <summary>
+        /// Вычисление
+        /// </summary>
+        /// <returns></returns>
+        double GetFigureArea()
+        {
+            return _figure.CalculateArea();
+        }
+    }
+}
